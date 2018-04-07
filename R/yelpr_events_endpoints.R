0f53895224458471f99ff6f64399a2329dc379eb
OmaymaS <omaymas@users.noreply.github.com>
2018-04-08 01:22:35 +0200

    update event_search_featured and doc

--- R/yelpr_events_endpoints.R
+++ R/yelpr_events_endpoints.R
@@ -44,13 +44,26 @@ event_search <- function(api_key, ...){
 
 #' return the featured event for a given location. Featured events are chosen by Yelp's community managers.
 #'
+#' @param api_key string
+#' @param transaction_type currently "delivery"
+#' @param location Required, if latitude and longitude not provided. Specifies the combination of "address, neighborhood, city, state or zip, optional country" to be used while searching for events.
+#' @param latitude Required, if location not provided. Latitude of the location to search from.
+#' @param longitude Required, if location not provided. Longitude of the location to search from.
+#'
 #' @examples
 #' key <- "######"
-#' event_search_featured(api_key = key, parameters = list(location = 'New York'))
+#' event_search_featured(api_key = key, location = 'New York')
 #'
 #' @export
+#'
 
-event_search_featured <- function(api_key, parameters){
+event_search_featured <- function(api_key,
+                                  location = NULL,
+                                  latitude = NULL,
+                                  longitude = NULL){
+
+  parameters <-  parameters <- c(as.list(environment()))
+  parameters <-  parameters[2:length(parameters)]
 
   res <- GET("https://api.yelp.com/v3/events/featured",
              add_headers(Authorization = prepare_header(api_key)),
@@ -58,3 +71,12 @@ event_search_featured <- function(api_key, parameters){
 
   fromJSON(content(res, type = "text"))
 }
+
+# event_search_featured2 <- function(api_key, parameters){
+#
+#   res <- GET("https://api.yelp.com/v3/events/featured",
+#              add_headers(Authorization = prepare_header(api_key)),
+#              query = parameters)
+#
+#   fromJSON(content(res, type = "text"))
+# }
