535dd1c1cb01ef3096aee3f00dc7c008efdc3415
OmaymaS <omaymas@users.noreply.github.com>
2018-04-08 14:04:02 +0200

    update event_search_featured doc
    add @return values

--- R/yelpr_events_endpoints.R
+++ R/yelpr_events_endpoints.R
@@ -73,13 +73,39 @@ event_search <- function(api_key, ...){
 }
 
 
-#' return the featured event for a given location. Featured events are chosen by Yelp's community managers.
+#' return the featured event for a given location
+#'
+#' @description
+#' Featured events are chosen by Yelp's community managers
 #'
 #' @param api_key string
 #' @param transaction_type currently "delivery"
 #' @param location Required, if latitude and longitude not provided. Specifies the combination of "address, neighborhood, city, state or zip, optional country" to be used while searching for events.
 #' @param latitude Required, if location not provided. Latitude of the location to search from.
 #' @param longitude Required, if location not provided. Longitude of the location to search from.
+#'
+#' @return
+#' * attending_count: Number of Yelp users attending this event
+#' * category: The category alias of this event
+#' * cost: Cost of attending this event
+#' * cost_max: Maximum cost of this event
+#' * description: Description excerpt of this event
+#' * event_site_url: Yelp page of this event
+#' * id: Event id
+#' * image_url: Yelp image URL of this event
+#' * interested_count: Number of Yelp users interested in attending this event
+#' * is_canceled: Whether this event is canceled
+#' * is_free: Whether this event is free
+#' * is_official: Whether this event is created by a Yelp community manager
+#' * latitude: Latitude of this event
+#' * longitude: Longitude of this event
+#' * name: Name of this event
+#' * tickets_url: URL to buy tickets for this event
+#' * time_end: Time this event ends. Returns date and time in the following format - "YYYY-MM-DD HH-mm"
+#' * time_start: Time this event starts. Returns date and time in the following format - "YYYY-MM-DD HH-mm"
+#' * location: 	The location of this event, including address, city, state, zipcode and country
+#' * business_id: Yelp Business ID of this event. No ID is returned if a business is not associated with an event
+#' @md
 #'
 #' @examples
 #' key <- "######"
